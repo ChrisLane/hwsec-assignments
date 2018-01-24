5305a46a0180b7db41c6af7e794451c5c55f1300
Joe Groocock <frebib@gmail.com>
2018-01-24 12:20:05 +0000

    Add correct pbox implementation for 3/5 tests

--- assigment1/present/present_ref/crypto.c
+++ assigment1/present/present_ref/crypto.c
@@ -1,7 +1,7 @@
 #include "crypto.h"
-#include <stdbool.h>
 
-static void add_round_key(uint8_t pt[CRYPTO_IN_SIZE], const uint8_t key[CRYPTO_KEY_SIZE]) {
+static void
+add_round_key(uint8_t pt[CRYPTO_IN_SIZE], const uint8_t key[CRYPTO_KEY_SIZE]) {
     // XOR the state (pt) with the round key
     for (uint8_t i = 0; i < CRYPTO_IN_SIZE; i++) {
         pt[i] ^= key[i];
@@ -9,33 +9,43 @@ static void add_round_key(uint8_t pt[CRYPTO_IN_SIZE], const uint8_t key[CRYPTO_K
 }
 
 static const uint8_t sbox[16] = {
-        0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD, 0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2,
+        0xC, 0x5, 0x6, 0xB,
+        0x9, 0x0, 0xA, 0xD,
+        0x3, 0xE, 0xF, 0x8,
+        0x4, 0x7, 0x1, 0x2
+};
+static const uint8_t pbox[64] = {
+        0,  16, 32, 48, 1,  17, 33, 49,
+        2,  18, 34, 50, 3,  19, 35, 51,
+        4,  20, 36, 52, 5,  21, 37, 53,
+        6,  22, 38, 54, 7,  23, 39, 55,
+        8,  24, 40, 56, 9,  25, 41, 57,
+        10, 26, 42, 58, 11, 27, 43, 59,
+        12, 28, 44, 60, 13, 29, 45, 61,
+        14, 30, 46, 62, 15, 31, 47, 63
 };
 
 static void sbox_layer(uint8_t s[CRYPTO_IN_SIZE]) {
     for (uint8_t i = 0; i < CRYPTO_IN_SIZE; i++) {
-        int ln = s[0] & 0xf;
-        int un = (s[0] >> 4) & 0xf;
-        s[0] = sbox[ln] | (sbox[un] << 4);
+        uint8_t ln = (uint8_t) (s[i] & 0xf);
+        uint8_t un = (uint8_t) ((s[i] >> 4) & 0xf);
+        s[i] = sbox[ln] | (sbox[un] << 4);
     }
 }
 
-static bool get_bit(uint8_t a, uint8_t bit) {
-    return (bool) ((a >> bit) & 0x1);
-}
-
-static int copy_bit(int out, int pos, bool v) {
-    out &= (1 << pos);
-    out |= (v << pos);
-    return out;
-}
-
+// ~123216
 static void pbox_layer(uint8_t s[CRYPTO_IN_SIZE]) {
-    for (uint8_t i = 0; i < 63; i++) {
-        bool tmp = get_bit(s[i], i);
-        int j = (i / 4) + (i % 4) * 16;
-        copy_bit(s[i], j, tmp);
+    uint8_t out[CRYPTO_IN_SIZE] = {0};
+    for (uint8_t byt = 0; byt < CRYPTO_IN_SIZE; byt++) {
+        for (uint8_t bit = 0; bit < 8; bit++) {
+            // p is output bit in the 64 bits
+            register uint8_t p = pbox[(byt << 3) + bit];
+            out[p >> 3] |= ((s[byt] >> bit) & 0x1) << (p % 8);
+        }
     }
+    // Copy result back to s
+    for (uint8_t i = 0; i < CRYPTO_IN_SIZE; i++)
+        s[i] = out[i];
 }
 
 static void update_round_key(uint8_t key[CRYPTO_KEY_SIZE], const uint8_t r) {
@@ -67,9 +77,8 @@ static void update_round_key(uint8_t key[CRYPTO_KEY_SIZE], const uint8_t r) {
 }
 
 void crypto_func(uint8_t pt[CRYPTO_IN_SIZE], uint8_t key[CRYPTO_KEY_SIZE]) {
-    uint8_t i = 0;
 
-    for (i = 1; i <= 31; i++) {
+    for (uint8_t i = 1; i < 32; i++) {
         // Note +2 offset on key since output of keyschedule are upper 8 byte
         add_round_key(pt, key + 2);
         sbox_layer(pt);
