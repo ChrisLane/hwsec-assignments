63a6b81bf503e868c35d7189529cd16cf6f1bca7
Chris Lane <chris@chrislane.com>
2018-03-13 18:38:58 +0000

    Add working square-and-multiply implementation

--- assignment2/src/longnum_sam/crypto.c
+++ assignment2/src/longnum_sam/crypto.c
@@ -1,25 +1,33 @@
 #include "crypto.h"
 
-
-void crypto_func(ln_limb_t ln_s[CRYPTO_IN_SIZE_WORDS], ln_limb_t ln_n[CRYPTO_IN_SIZE_WORDS],
-                 ln_limb_t ln_mu[CRYPTO_IN_SIZE_WORDS + 1], const ln_limb_t exp, ln_limb_t ln_r[CRYPTO_IN_SIZE_WORDS]) {
+void crypto_func(ln_limb_t x[CRYPTO_IN_SIZE_WORDS], ln_limb_t n[CRYPTO_IN_SIZE_WORDS],
+                 ln_limb_t ln_mu[CRYPTO_IN_SIZE_WORDS + 1], const ln_limb_t exp,
+                 ln_limb_t y[CRYPTO_IN_SIZE_WORDS]) {
     // Helper variables
-    ln_limb_t tmp[2 * CRYPTO_IN_SIZE_WORDS], sp[2 * CRYPTO_IN_SIZE_WORDS + 2];
+    ln_limb_t tmp[2 * CRYPTO_IN_SIZE_WORDS], scratch[2 * CRYPTO_IN_SIZE_WORDS + 2];
 
     // Length of exponent in bit
-    int8_t e_l = 15;
+    int8_t exp_len = 15;
 
     // Determine MSBit position
-    while (e_l > 0 && (exp & (1 << e_l)) == 0) {
-        e_l--;
+    while (exp_len > 0 && (exp & (1 << exp_len)) == 0) {
+        exp_len--;
     }
 
-    // Example code for modular reduction with Barrett method:
-    // ln_mod_barrett(tmp, ln_n, ln_mu, CRYPTO_IN_SIZE_WORDS, ln_r, sp, 2*CRYPTO_IN_SIZE_WORDS + 2);
-    // Reduces the double-length long number in tmp modulo ln_n, uses the helper variable mu
-    // Stores the result in ln_r
-    // Requires the scratchpad variable defined above
+    // y <- x
+    ln_assign(y, x, CRYPTO_IN_SIZE_WORDS);
 
-    // Insert your S-a-M code here
+    // Lecture notes say "i = t - 2" here, somehow that didn't work but "i = t - 1" did
+    for (int i = exp_len - 1; i >= 0; i--) {
+        // x <- x^2 mod n
+        ln_multiply(y, y, tmp, CRYPTO_IN_SIZE_WORDS);
+        ln_mod_barrett(tmp, n, ln_mu, CRYPTO_IN_SIZE_WORDS, y, scratch, 2 * CRYPTO_IN_SIZE_WORDS + 2);
 
+        // Check if ei == 1
+        if (((exp << i) & 0x1) == 1) {
+            // y <- y * x mod n
+            ln_multiply(y, x, tmp, CRYPTO_IN_SIZE_WORDS);
+            ln_mod_barrett(tmp, n, ln_mu, CRYPTO_IN_SIZE_WORDS, y, scratch, 2 * CRYPTO_IN_SIZE_WORDS + 2);
+        }
+    }
 }
